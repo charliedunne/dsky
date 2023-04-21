a12e72680ff4c103f218bcabc98305b74d2852ea
Charlid Dunn'e <charlie.dunne@gmail.com>
2023-04-21 20:59:06 +0200

    Update build script to compile and install GFX library

--- sw/3rdparty/build.sh
+++ sw/3rdparty/build.sh
@@ -3,9 +3,21 @@
 ROOT=$(pwd)
 
 # BUILD SDL
-cd SDL
-cmake -S . -B ./build -DCMAKE_INSTALL_PREFIX:PATH=${ROOT}/../build
-cd build
-cmake --build  ./
-cmake --install .
-cd ..
+#cd SDL
+#cmake -S . -B ./build -DCMAKE_INSTALL_PREFIX:PATH=${ROOT}/../build
+#cd build
+#cmake --build  ./
+#cmake --install .
+#cd ..
+#cd ..
+
+# BUILD GFX
+tar -xzvf SDL2_gfx-1.0.4.tar.gz
+cd SDL2_gfx-1.0.4
+./configure --prefix=${ROOT}/../build
+make
+make install
+mkdir -p ${ROOT}/../build/include/GFX
+cp *.h ${ROOT}/../build/include/GFX/
+cd..
+
