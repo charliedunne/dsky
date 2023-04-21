86cc8f32509e46e8a9470939efe1e1f96bc5ded2
pi <charlie.dunne@gmail.com>
2023-04-22 01:27:52 +0200

    Update 3rd Party build script to support ARM compilation for GFX

--- sw/3rdparty/build.sh
+++ sw/3rdparty/build.sh
@@ -1,23 +1,65 @@
 #!/bin/bash
 
 ROOT=$(pwd)
+ARCH=$(uname -m)
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: ./build.sh ALL|SDL|GFX"
+    exit
+fi
 
 # BUILD SDL
-#cd SDL
-#cmake -S . -B ./build -DCMAKE_INSTALL_PREFIX:PATH=${ROOT}/../build
-#cd build
-#cmake --build  ./
-#cmake --install .
-#cd ..
-#cd ..
+function build_sdl() {
+
+    echo "SDL Building..."
+
+    cd SDL
+    cmake -S . -B ./build -DCMAKE_INSTALL_PREFIX:PATH=${ROOT}/../build
+    cd build
+    cmake --build  ./
+    cmake --install .
+    cd ..
+    cd ..
+}
 
 # BUILD GFX
-tar -xzvf SDL2_gfx-1.0.4.tar.gz
-cd SDL2_gfx-1.0.4
-./configure --prefix=${ROOT}/../build
-make
-make install
-mkdir -p ${ROOT}/../build/include/GFX
-cp *.h ${ROOT}/../build/include/GFX/
-cd..
+function build_gfx() {
+
+    echo "GFX Building..."
+
+    tar -xzvf SDL2_gfx-1.0.4.tar.gz
+    cd SDL2_gfx-1.0.4
+    if [ ${ARCH} == "aarch64" ]
+    then
+	# RAPBIAN (aarch64)
+	./configure --prefix=${ROOT}/../build --build=aarch64-unknown-linux --disable-mmx
+    else
+	# Linux (x86_64)
+	./configure --prefix=${ROOT}/../build
+    fi
+    make
+    make install
+    mkdir -p ${ROOT}/../build/include/GFX
+    cp *.h ${ROOT}/../build/include/GFX/
+    cd ..
+}
+
+
+if [ $1 == "ALL" ]
+then
+    build_sdl
+    build_gfx
+
+elif [ $1 == "SDL" ]
+then
+    build_sdl
+
+elif [ $1 == "GFX" ]
+then
+     build_gfx
+
+fi
+
+exit
 
