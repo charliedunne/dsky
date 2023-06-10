e21375d626fc9297eff15886179fd072a23c701b
Charlid Dunn'e <charlie.dunne@gmail.com>
2023-06-10 23:51:48 +0200

    Add Parse fnt functionality Fix bug when reading PNG

--- sw/src/hmi/Font.cpp
+++ sw/src/hmi/Font.cpp
@@ -3,6 +3,7 @@
 // STL
 #include <stdexcept>
 #include <fstream>
+#include <regex>
 
 // SDL
 #include "SDL2/SDL_image.h"
@@ -18,9 +19,9 @@ Font::Font(SDL_Renderer *r, std::string bitmap, std::string fnt)
     fnt_ = fnt;
 
     // Create the texture
-    SDL_Surface *surface = IMG_Load(fnt.c_str());
+    SDL_Surface *surface = IMG_Load(bitmap.c_str());
+    if (surface == NULL)
     {
-
         LogError << "Fail loading the Segment texture (" << bitmap << "): " << SDL_GetError() << std::endl;
         throw std::domain_error("Invalid Bitmap for Font");
     }
@@ -46,12 +47,76 @@ Font::~Font()
 
 void Font::parseFnt()
 {
-    
+
     std::ifstream infile(fnt_.c_str());
 
     std::string line;
 
     while (std::getline(infile, line))
     {
+        charCoords_t coords;
+
+        std::vector<std::string> elements;
+        const char delim = ' ';
+
+        char *token = std::strtok(const_cast<char *>(line.c_str()), &delim);
+        while (token != nullptr)
+        {
+            std::string s = std::string(token);
+
+            // ID
+            if (std::regex_match(s, std::regex("^id=.*")))
+            {
+                coords.id = std::stoi(s.substr(3));
+            }
+
+            // X
+            if (std::regex_match(s, std::regex("^x=.*")))
+            {
+                coords.x = std::stoi(s.substr(2));
+            }
+
+            // Y
+            if (std::regex_match(s, std::regex("^y=.*")))
+            {
+                coords.y = std::stoi(s.substr(2));
+            }
+
+            // Width
+            if (std::regex_match(s, std::regex("^width=.*")))
+            {
+                coords.w = std::stoi(s.substr(6));
+            }
+
+            // Height
+            if (std::regex_match(s, std::regex("^height=.*")))
+            {
+                coords.h = std::stoi(s.substr(7));
+            }
+
+            // xoffset
+            if (std::regex_match(s, std::regex("^xoffset=.*")))
+            {
+                coords.xoffset = std::stoi(s.substr(8));
+            }
+
+            // yoffset
+            if (std::regex_match(s, std::regex("^yoffset=.*")))
+            {
+                coords.yoffset = std::stoi(s.substr(8));
+            }
+
+            // xadvance
+            if (std::regex_match(s, std::regex("^xadvance=.*")))
+            {
+                coords.yoffset = std::stoi(s.substr(9));
+            }
+
+            // Add parameters to coords list
+            coords_.push_back(coords);
+
+            // Move to next sub-line element
+            token = std::strtok(nullptr, &delim);
+        }
     }
 }
--- sw/tests/Font/main.cpp
+++ sw/tests/Font/main.cpp
@@ -1,15 +1,47 @@
 #include "Font.h"
 
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
+#include "config.h"
 
 #include <string>
+#include "Logger.h"
 
 int main()
 {
-    SDL_Renderer *r = nullptr;
+
+    //    IMG_Init(IMG_INIT_PNG);
+
+    // Window
+    SDL_Window *win_ = NULL;
+
+    // Renderer
+    SDL_Renderer *ren_ = NULL;
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        LogError << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    }
+
+    // Create the window
+    win_ = SDL_CreateWindow("DSKY", 100, 100, 800, 640, SDL_WINDOW_SHOWN);
+    if (win_ == NULL)
+    {
+        LogError << "SDL_CreateWindow()" << std::endl;
+    }
+
+    // Create the renderer
+    ren_ = SDL_CreateRenderer(win_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren_ == NULL)
+    {
+        LogError << "SDL_CreateRenderer()" << std::endl;
+    }
 
     std::string bitmap = "../../../resources/fonts/Gorton-Condensed_32px.png";
+    // std::string bitmap = DIGIT_TXT_FILE;
     std::string fnt = "../../../resources/fonts/Gorton-Condensed_32px.fnt";
 
-    Font(r, bitmap, fnt);
+    Font(ren_, bitmap, fnt);
 }
